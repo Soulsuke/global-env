a60d740f5ec1800f96def77a2eaf4975517ac3be
Maurizio Oliveri <6tsukiyami9@gmail.com>
2024-07-05 14:09:31 +0200

    Updated mic toggling script.

--- scripts/wpctl_all_microphones_set_mute.zsh
+++ scripts/wpctl_all_microphones_set_mute.zsh
@@ -1,7 +1,21 @@
 #! /usr/bin/env zsh
 
+# Command must be either 0, 1 or 'toggle'.
 CMD=${1}
 
+# Toggle is tricky, because pluggnig/unplugging mics may cause different states
+# between devices. So:
+if [[ ${CMD} == "toggle" ]]; then
+  # If the main device is muted, unmute everything:
+  if wpctl status | grep -E '\*.*Microphone.*MUTED' &> /dev/null; then
+    CMD=0
+  # Otherwise, mute everything:
+  else
+    CMD=1
+  fi
+fi
+
+# Set the state for all devices:
 wpctl status | grep Microphone | \
   awk "/│/ {gsub(\"[*.]\",\"\"); system(\"wpctl set-mute \" \$2 \" ${CMD}\")}"
 
