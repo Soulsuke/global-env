27f195f21aed1eb70bbf60ad2fe4fdd72c8f009f
Maurizio Oliveri <6tsukiyami9@gmail.com>
2025-02-24 15:42:50 +0100

    Made hyprlock.zsh handle screen dimming and dunst notifications. Simplified hypridle.conf.

--- scripts/hyprlock.zsh
+++ scripts/hyprlock.zsh
@@ -1,14 +1,41 @@
 #! /usr/bin/env zsh
 
-# Variables:
-local HOST_CONFIG="${HOME}/.config/hypr/hyprlock.conf.hosts/${HOST}.conf"
+### Utility functions
+###############################################################################
 
-# If we have a per-host configuration, use it:
-if [[ -f "${HOST_CONFIG}" ]]; then
-  hyprlock --config "${HOST_CONFIG}"
+# Starts hyprlock, managing dunst notifications.
+function start_hyprlock()
+{
+  # Variables:
+  local HOST_CONFIG="${HOME}/.config/hypr/hyprlock.conf.hosts/${HOST}.conf"
 
-# Otherwise, use the default one:
-else
-  hyprlock
-fi
+  # Stop dunst notifications:
+  dunstctl set-paused true
+
+  # If we have a per-host configuration, use it:
+  if [[ -f "${HOST_CONFIG}" ]]; then
+    hyprlock --config "${HOST_CONFIG}"
+
+  # Otherwise, use the default one:
+  else
+    hyprlock
+  fi
+
+  # Start dunst notification again:
+  dunstctl set-paused false
+}
+
+
+
+### Entry point
+###############################################################################
+
+# Start hyprlock in the background:
+start_hyprlock &
+
+# Give it a second:
+sleep 1
+
+# Then dim the screen:
+hyprctl dispatch dpms off
 
