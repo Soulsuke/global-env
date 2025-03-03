4f5cb299ecd3140c3d7368155e57e574c5bbd10f
Maurizio Oliveri <6tsukiyami9@gmail.com>
2025-03-03 08:55:06 +0100

    Moved some logic from hypridel.conf to hyprlock.zsh

--- scripts/hyprlock.zsh
+++ scripts/hyprlock.zsh
@@ -9,9 +9,6 @@ function start_hyprlock()
   # Variables:
   local HOST_CONFIG="${HOME}/.config/hypr/hyprlock.conf.hosts/${HOST}.conf"
 
-  # Stop dunst notifications:
-  dunstctl set-paused true
-
   # If we have a per-host configuration, use it:
   if [[ -f "${HOST_CONFIG}" ]]; then
     hyprlock --config "${HOST_CONFIG}"
@@ -21,7 +18,8 @@ function start_hyprlock()
     hyprlock
   fi
 
-  # Start dunst notification again:
+  # This gets called only when hyprlock has effectively stopped.
+  # Restore dunst notifications:
   dunstctl set-paused false
 }
 
@@ -30,8 +28,11 @@ function start_hyprlock()
 ### Entry point
 ###############################################################################
 
-# Start hyprlock in the background:
-start_hyprlock &
+# Disable dunst notifications:
+dunstctl set-paused true
+
+# Start hyprlock in the background, only if it isn't already running:
+pidof hyprlock || start_hyprlock &
 
 # Give it a second:
 sleep 1
