783a916391c0aa16df243019d25ab1a4f5937a74
Maurizio Oliveri <6tsukiyami9@gmail.com>
2025-03-28 09:49:15 +0100

    Added hyprsunset at startup.

--- scripts/startup/00-session-startup.zsh
+++ scripts/startup/00-session-startup.zsh
@@ -54,6 +54,7 @@ case ${XDG_SESSION_DESKTOP:l} in
     dbus-update-activation-environment \
       --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
     hypridle &
+    hyprsunset &
     ${HOME}/.scripts/7shi/waybar.zsh &
     ${HOME}/.scripts/7shi/wallpaper.zsh &
     ${HOME}/.scripts/7shi/dunst.zsh &
