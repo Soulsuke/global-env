1d095d13380c86328048ce5d2b3b5c1d42fa4dc1
Maurizio Oliveri <6tsukiyami9@gmail.com>
2025-04-14 12:08:18 +0200

    Edited wf-recorder.zsh to capture all audio.

--- config/waybar/modules/custom/wf-recorder/wf-recorder.zsh
+++ config/waybar/modules/custom/wf-recorder/wf-recorder.zsh
@@ -17,12 +17,34 @@ else
 
   # And start recording:
   if [[ ${1} == "toggle" ]]; then
+    # Let's use a unique sink name:
+    local SINK="wf_recorder_$(date "+%s")"
+
+    # Create a compiste sink for wf-recorder:
+    pw-loopback \
+      -m "[ FL FR FC LFE RL RR ]" \
+      --capture-props="media.class=Audio/Sink node.name=${SINK}" \
+      --playback-props='target.object="my-default-sink"' &
+
+    # Get its pid:
+    local LOOPBACK=$!
+
+    # -a -> audio sink name
+    # -C -> audio codec
+    # -c -> video codec
+    # -g -> screen area to record
+    # -y -> force overwrite if file exists (should never happen, buuuuut...)
+    # -f -> output file name
     wf-recorder \
-      -a \
+      -a "${SINK}" \
+      -C ac3_fixed \
+      -c hevc_vaapi \
       -g "$(slurp)" \
       -y \
-      -f "$(xdg-user-dir VIDEOS)/wf-recorder_$(date "+%Y-%m-%d_%H-%M-%S").mkv" \
-      &> /dev/null &
+      -f "$(xdg-user-dir VIDEOS)/wf-recorder_$(date "+%Y-%m-%d_%H-%M-%S").mkv"
+
+    # Once the recording ends, stop the loopback:
+    kill ${LOOPBACK}
   fi
 fi
 
