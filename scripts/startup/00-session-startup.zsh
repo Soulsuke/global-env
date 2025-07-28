df8557b7d49ea6fabcdd090cd1a80868d53bd840
Maurizio Oliveri <6tsukiyami9@gmail.com>
2025-07-28 09:05:01 +0200

    Will now only start redshift on x11.

--- scripts/startup/00-session-startup.zsh
+++ scripts/startup/00-session-startup.zsh
@@ -71,7 +71,7 @@ esac
 
 # Common for Every DE, after the compositor is in place:
 gnome-keyring-daemon --start
-redshift-gtk &
+[[ ${XDG_SESSION_TYPE:l} == "x11" ]] && redshift-gtk &
 [[ $(command -v akbl) ]] && akbl --start-indicator &
 ${HOME}/.scripts/7shi/wpctl_set_mute_all.zsh Sources 1
 ${HOME}/.scripts/7shi/wpctl_set_mute_all.zsh Sinks 0
