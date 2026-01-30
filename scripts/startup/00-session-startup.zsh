d78666981afa0e1b682c97e80fb05389a2ac8b69
Maurizio Oliveri <6tsukiyami9@gmail.com>
2026-01-30 09:45:11 +0100

    Removed awcc.

--- scripts/startup/00-session-startup.zsh
+++ scripts/startup/00-session-startup.zsh
@@ -76,5 +76,4 @@ ${HOME}/.scripts/7shi/wpctl_set_mute_all.zsh Sinks 0
 [[ ${XDG_SESSION_TYPE:l} == "x11" ]] && redshift-gtk &
 [[ $(command -v akbl) ]] && akbl --start-indicator &
 [[ $(command -v kdeconnect-indicator) ]] && kdeconnect-indicator &
-[[ $(command -v awcc) ]] && awcc b &
 
