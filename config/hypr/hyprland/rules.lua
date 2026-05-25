b8f2c8c23f02b63dec6b51fd9c8f7691cebb1095
Maurizio Oliveri <6tsukiyami9@gmail.com>
2026-05-25 17:27:56 +0200

    Fixed steam rules.

--- config/hypr/hyprland/rules.lua
+++ config/hypr/hyprland/rules.lua
@@ -510,16 +510,6 @@ hl.window_rule(
 
 hl.window_rule(
   {
-    name = "w10-steam-title",
-    workspace = "10 silent",
-    match = {
-      title = "^Steam$"
-    }
-  }
-)
-
-hl.window_rule(
-  {
     name = "w10-launchers",
     workspace = "10 silent",
     match = {
