7f349a56ecbdf159d9edea0785d5ed626a0c9478
Maurizio Oliveri <6tsukiyami9@gmail.com>
2026-08-21 14:51:51 +0200

    Somehow display name changed.

--- config/hypr/hyprland/hosts/chunchunmaru.lua
+++ config/hypr/hyprland/hosts/chunchunmaru.lua
@@ -2,7 +2,7 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor(
   {
-    output = "eDP-2",
+    output = "eDP-1",
     mode = "2560x1600@240",
     position = "0x0",
     scale = 1.25
